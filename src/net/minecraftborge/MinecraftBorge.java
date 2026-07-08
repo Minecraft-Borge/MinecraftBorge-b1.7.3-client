e40625f713f006af203c177d08408cba956e8c44
Siepert123 <createlegacy69@gmail.com>
2026-07-08 19:38:38 +0200

    1.7.0 Ingredient improvements Grass texture fix

--- src/net/minecraftborge/MinecraftBorge.java
+++ src/net/minecraftborge/MinecraftBorge.java
@@ -37,7 +37,7 @@ public class MinecraftBorge {
 	private static boolean useFallback = false;
 	public static boolean hasLoaded = false;
 
-	public static final String VERSION = "1.6.4";
+	public static final String VERSION = "1.7.0";
 	public static final String MC_VERSION = "b1.7.3";
 	public static String NEW_VERSION = null;
 
